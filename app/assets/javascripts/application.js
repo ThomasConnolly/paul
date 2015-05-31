d5d9dd78941ec9d0fb224d8b69fa0dc966ec2730
ThomasConnolly <tom.connolly@comcast.net>
2015-05-30 21:45:09 -0400

    trying to fix tinymce on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/dropdown
+//= require bootstrap
 //= require_tree .
 //= require cloudinary
 //= require attachinary

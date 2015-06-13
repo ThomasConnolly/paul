a141380b56b18d228cbb24477009658ed5abf48d
ThomasConnolly <tom.connolly@comcast.net>
2015-06-13 19:06:43 -0400

    delete mailers

--- app/mailers/application_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "welcome@saintpaulsnaples.org", to: { User.pluck(:email) } 
-
-  def welcome_email(user)
-    @user = user
-    mail(to:@user.email, subject: "welcome")
-  end
-end

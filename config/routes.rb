b4e41a83c4fb89450df58b106737e97479f7aa0b
ThomasConnolly <tom.connolly@comcast.net>
2015-08-19 16:19:32 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   resources :books
   resources :pledges
   resources :sermons
-  devise_for :users, controllers => { :registrations => "my_registrations"}
+  devise_for :users, controllers: { :registrations => 'my_registrations' }
     devise_scope :user do
       get 'register', to: 'devise/registrations#new', as: :register
       get 'login', to: 'devise/sessions#new', as: :login

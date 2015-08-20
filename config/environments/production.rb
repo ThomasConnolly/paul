f6a15e14e840b3bc56089a65bd438e4978d62f94
ThomasConnolly <tom.connolly@comcast.net>
2015-08-19 23:37:20 -0400

    added host info to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ config.log_level = :info
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default_url_options = { host: 'saintpauls.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {

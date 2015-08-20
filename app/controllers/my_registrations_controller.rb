52ab67c8f9e0612b51bc3c18276689dac2b7f655
ThomasConnolly <tom.connolly@comcast.net>
2015-08-20 10:02:51 -0400

    modify my_reg controller to fix mailer

--- app/controllers/my_registrations_controller.rb
+++ app/controllers/my_registrations_controller.rb
@@ -2,10 +2,6 @@ class MyRegistrationsController < Devise::RegistrationsController
 
   def create
     super
-    if @user.save
-      WelcomeMailer.welcome_email(@user).deliver_now
-    else
-      render 'new'
-    end
+      WelcomeMailer.welcome_email(@user).deliver unless @user.invalid?
   end
 end

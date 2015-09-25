4fa170746d23054a48cd3de22d44351459f18fc8
ThomasConnolly <tom.connolly@comcast.net>
2015-09-25 15:57:55 -0400

    fixed ajax with posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,55 +1,43 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
- 
-  def index
-    @posts = Post.all.includes(:comments)
-  end
+  before_action :set_posts, only: [:edit, :update, :destroy]
+  before_action :all_posts, only: [:index, :create, :update, :destroy]
   
-  def show
-    @post = Post.find(params[:id])
-    @comment = Comment.new(:post => @post)
-  end
+  respond_to :html, :js
+
 
   def new
     @post = Post.new
   end
-  
+
   def create
-    @post = Post.new(post_params)
-    @post.user = current_user
-      if @post.save
-        redirect_to posts_path
-      else
-        render 'new'
-    end
-  end
-  
-  def edit
+    @post = current_user.posts.build(post_params)
+    @post.save
   end
 
   def update
-    if @post.update_attributes(post_params)
-      redirect_to posts_path
-    else
-      render 'edit'
-    end
+    @post.update_attributes(post_params)
   end
 
   def destroy
     @post.destroy
-      redirect_to posts_path, notice: 'Post was removed'
-    end
-  
+  end
+
+
+
 
 
 private
 
-  def post_params
-    params.require(:post).permit(:content, :user_id, :post_picture, photos: [])
-    end
+  def all_posts
+    @posts = Post.all
+  end
 
-  def set_post
+  def post_params
+    params.require(:post).permit(:content, :post_picture, photos: [])
+  end
+
+  def set_posts
     @post = Post.find(params[:id])
   end
 end

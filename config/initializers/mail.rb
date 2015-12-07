35769f4616b280abacec92dcd56e105a7a584976
ThomasConnolly <tom.connolly@comcast.net>
2015-12-06 23:07:41 -0500

    still working on sendgrid email

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'saintpauls.herokuapp.com',
+  :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
 

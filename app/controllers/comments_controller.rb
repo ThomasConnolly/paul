eb35b4f306bd70e3451963d94889a1f44ae370f8
ThomasConnolly <tom.connolly@comcast.net>
2015-12-23 14:07:29 -0500

    need to update the home page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,29 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
-  
+  #  def new
+  #  end
 
-  def index
-    @commentable = find_commentable
-    @comments = @commentable.all
-    @comment = @commentable.comments.new
-  end
+  #  def index
+    #  @commentable = find_commentable
+    #  @comments = @commentable.all
+    #  @comment = @commentable.comments.new
+  #  end
 
   def create
     @comment = @commentable.comments.build comment_params
     @comment.user = current_user
     @comment.save
-    redirect_to :back
+      redirect_to :back 
   end
+    
 
-   def delete
+
+   def destroy
+    @comment = Comment.find(params[:id])
+    @commentable = @comment.commentable
+    @comment.destroy
+    redirect_to :back
    end
 
 

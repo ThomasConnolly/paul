3d7803d69676b739a9f305147b7d55046a0d75b9
ThomasConnolly <tom.connolly@comcast.net>
2016-02-19 14:15:51 -0500

    update home page, vreports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207135303) do
+ActiveRecord::Schema.define(version: 20160219150029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,8 +147,6 @@ ActiveRecord::Schema.define(version: 20160207135303) do
     t.string   "full_name",              limit: 255
     t.integer  "role"
     t.string   "stripe_customer_id"
-    t.date     "birthday"
-    t.date     "anniversary"
   end
 
   add_index "users", ["full_name"], name: "index_users_on_full_name", using: :btree

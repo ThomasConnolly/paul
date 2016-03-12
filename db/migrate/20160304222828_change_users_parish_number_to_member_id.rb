d0917852c631d953b29c5475dc6e75245ed51a97
ThomasConnolly <tom.connolly@comcast.net>
2016-03-12 09:58:08 -0500

    adding member id to user, heroku only

--- db/migrate/20160304222828_change_users_parish_number_to_member_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUsersParishNumberToMemberId < ActiveRecord::Migration
-  def change
-    add_column :users, :member_id, :integer
-    remove_column :users, :ParishNumber, :string    
-  end
-end

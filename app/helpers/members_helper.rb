17bce7918337b99dff76cfd5c30e98dabcde42f2
TomConnolly <tedwardconnolly@gmail.com>
2016-04-24 12:52:23 -0400

    fix birthdays end of week

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -19,6 +19,6 @@ module MembersHelper
 
   def birthdays_this_week
     Member.find_birthdays_for(Date.today.beginning_of_week(start_day = :sunday), 
-      Date.today.end_of_week).order(:birthday)
+      Date.today.end_of_week(start_day= :sunday)).order(:birthday)
   end
 end

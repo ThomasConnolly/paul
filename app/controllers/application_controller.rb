96397fd9d83af5c9d1011ef074ba6196eb6a44e1
TomConnolly <tedwardconnolly@gmail.com>
2016-07-09 13:22:05 -0400

    update devise sanitize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,11 @@ protected
   
   def configure_permitted_parameters
    
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, 
-      :password, :password_confirmation, :current_password) }
-    devise_parameter_sanitizer.for(:accept_invitation) { |u| u.permit(:first_name, :last_name) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, 
+      :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name])
   end
 
 end

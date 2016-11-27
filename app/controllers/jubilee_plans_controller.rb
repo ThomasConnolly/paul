2eda866e3f183b5309445a57f8e89fe2afb41538
TomConnolly <tedwardconnolly@gmail.com>
2016-11-27 01:12:05 -0500

    wondering if restart will fix issue

--- app/controllers/jubilee_plans_controller.rb
+++ app/controllers/jubilee_plans_controller.rb
@@ -28,7 +28,7 @@ class JubileePlansController < ApplicationController
   # POST /jubilees
   # POST /jubilees.js
   def create
-    @jubilee_plan = JubileePlan.new(jubilee_plan_params)
+    @jubilee_plan = JubileePlan.new(params[:jubilee_plan])
     @jubilee_plan.user = current_user
       if @jubilee_plan.save
         JubileePlanMailer.jubilee_plan_created(@jubilee_plan).deliver_later

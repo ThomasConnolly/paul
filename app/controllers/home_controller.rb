5195bcd23cf95bd94fd32d8d216c9dc1eb157f4b
ThomasConnolly <tom.connolly@comcast.net>
2017-04-06 17:44:03 -0400

    move tumaini off home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
    @meditations = Meditation.all
    @meditation = Meditation.publish_on_today.first
+   @prayers = Prayer.all
   end
 
   def invitation
---
+++ app/helpers/prayers_helper.rb
@@ -0,0 +1,13 @@
+require 'date'
+
+module PrayersHelper
+
+  def start_date
+    Date.parse("2017-04-12")
+  end
+
+  def end_date
+    Date.parse("2017-04-15")
+  end
+end
+

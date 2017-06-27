8e1bd236de4de802a6f922704da68b7b2c7944be
Thomas Connolly <github email address>
2017-06-27 18:47:26 -0400

    more work on google cover artwork

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608042211) do
+ActiveRecord::Schema.define(version: 20170627213204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170608042211) do
     t.datetime "updated_at"
     t.text "description"
     t.string "cutter"
+    t.string "url"
     t.index ["author", "title"], name: "index_books_on_author_and_title"
   end
 

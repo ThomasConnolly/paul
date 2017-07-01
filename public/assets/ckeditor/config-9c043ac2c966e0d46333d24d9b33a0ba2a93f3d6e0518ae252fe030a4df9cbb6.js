3274da2eae51aa68cd1d5b78d02e2c97f0c6531e
Thomas Connolly <github email address>
2017-07-01 14:31:25 -0400

    precompiling after deleting webpacker gem

---
+++ public/assets/ckeditor/config-9c043ac2c966e0d46333d24d9b33a0ba2a93f3d6e0518ae252fe030a4df9cbb6.js
@@ -0,0 +1,20 @@
+CKEDITOR.editorConfig = function(config) 
+{
+  config.language = 'en';
+  config.width = "100%";
+  config.height = "100%";
+  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+  config.toolbar = [
+    { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source'] },
+    { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'] },
+    { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] }, 
+    { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight']},
+    { name: 'links', items: ['Link', 'Unlink']}, 
+    { name: 'insert', items: ['Table', 'HorizontalRule']}
+  ];
+};

0700a7a6bc1fd7b2be06fdd2a5f53407ccf473c4
ThomasConnolly <tom.connolly@comcast.net>
2018-06-07 10:34:45 -0400

    still on security

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,6 +5,7 @@ Rails.application.config.content_security_policy do |policy|
     policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035"
     #policy.script_src :self, :https, :unsafe_eval
   else
-    policy.script_src :self, :https, unsafe-inline
+    policy.script_src :self, :https
+    
   end
 end

4c43d402b55d3493937987107a466455c410a000
ThomasConnolly <tom.connolly@comcast.net>
2019-01-24 11:22:32 -0500

    typo in migration

--- db/migrate/20190120131640_change_full_name_to_username_in_users.rb
+++ db/migrate/20190120131640_change_full_name_to_username_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeFullNameToUsernameInUsers < ActiveRecord::Migration[5.2]
   def change
-    rename_column :users, :username, :username
+    rename_column :users, :full_name, :username
   end
 end

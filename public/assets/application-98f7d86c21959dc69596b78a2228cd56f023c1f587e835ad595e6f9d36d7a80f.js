b223652850e9ba11ab6a141bc2dfd645975fa702
ThomasConnolly <tom.connolly@comcast.net>
2019-02-04 15:52:12 -0500

    precompiling assets

--- public/assets/application-98f7d86c21959dc69596b78a2228cd56f023c1f587e835ad595e6f9d36d7a80f.js
+++ public/assets/application-98f7d86c21959dc69596b78a2228cd56f023c1f587e835ad595e6f9d36d7a80f.js
@@ -35231,6 +35231,70 @@ hooks.HTML5_FMT = {
 return hooks;
 
 })));
+$( document ).on('ready, turbolinks:load', function() {
+  var stripe = Stripe('pk_test_i3Ry2iGGqQIXZiVwJ2eimNOM');
+  var elements = stripe.elements();
+
+  document.addEventListener("turbolinks:load", function() {
+    if (!document.querySelector("#card-element")) {
+        return;
+  }
+
+  var style = {
+    base: {
+      fontSize: '24px'
+    }
+  };
+  var card = elements.create('card', {style: style});
+  card.mount('#card-element');
+
+  card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+      displayError.textContent = event.error.message;
+    } else {
+      displayError.textContent = '';
+     }
+  });
+
+  var form = document.getElementById('albergue-payment-form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    stripe.createToken(card).then(function(result) {
+      if (result.error) {
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      stripeTokenHandler(result.token);
+      }
+    });
+  });
+});
+
+function stripeTokenHandler(token) {
+  var form = document.getElementById('albergue-payment-form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  ["brand", "exp_month", "exp_year", "last4"].forEach(function(field) {
+    addFieldToForm(form, token, field);
+  });
+
+  form.submit();
+}
+
+ function addFieldToForm(form, token, field){
+   var hiddenInput = document.createElement('input');
+   hiddenInput.setAttribute('type', 'hidden');
+   hiddenInput.setAttribute('name', "[card_" + field + "]");
+   hiddenInput.setAttribute('value', token.card[field]);
+   form.appendChild(hiddenInput);
+ }
+});
 
   var CKEDITOR_BASEPATH = '/assets/ckeditor/';
 (function() {
@@ -35255,12 +35319,12 @@ $( document ).on('ready, turbolinks:load', function() {
 
   var style = {
     base: {
-      fontSize: '24px',
-      lineHeight: '36px'
+      fontSize: '24px'
     }
   };
   var card = elements.create('card', {style: style});
   card.mount('#card-element');
+
 
   card.addEventListener('change', function(event) {
     var displayError = document.getElementById('card-errors');
@@ -35286,28 +35350,28 @@ $( document ).on('ready, turbolinks:load', function() {
   });
 });
 
-function stripeTokenHandler(token) {
-  var form = document.getElementById('payment-form');
-  var hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
-  form.appendChild(hiddenInput);
+  function stripeTokenHandler(token) {
+    var form = document.getElementById('payment-form');
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
 
-  ["brand", "exp_month", "exp_year", "last4"].forEach(function(field) {
-    addFieldToForm(form, token, field);
-  });
+    ["brand", "exp_month", "exp_year", "last4"].forEach(function(field) {
+      addFieldToForm(form, token, field);
+    });
 
   form.submit();
-}
+  }
 
  function addFieldToForm(form, token, field){
    var hiddenInput = document.createElement('input');
    hiddenInput.setAttribute('type', 'hidden');
-   hiddenInput.setAttribute('name', "user[card_" + field + "]");
+   hiddenInput.setAttribute('name', "[card_" + field + "]");
    hiddenInput.setAttribute('value', token.card[field]);
    form.appendChild(hiddenInput);
- }
+}
 ;
 /*document.addEventListener("turbolinks:load", function() {
   var public_key = document.querySelector("meta[name='stripe-public-key']").content;

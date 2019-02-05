02e2982893e2aab363e62cae943b5ad897893f2b
ThomasConnolly <tom.connolly@comcast.net>
2019-02-04 21:03:10 -0500

    issue may be with stripe webhooks

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 # original_formatter = Logger::Formatter.new
-# Rails.logger.formatter = proc { |severity, datetime, progname, msg|
+# Rails Starting process with command `bundle exec puma -C config/puma.rb` 2019-02-05T01:42:57.563241+00:00 app[web.1]: ! Unable to load application: ArgumentError: Before process_action callback :verify_authenticity_token has not b Starting process with command `bundle exec puma -C config/puma.rb` 2019-02-05T01:42:57.563241+00:00 app[web.1]: ! Unable to load application: ArgumentError: Before process_action callback :verify_authenticity_token has not been defined 2019-02-05T01:42:57.563378+00:00 app[web.1]: een defined 2019-02-05T01:42:57.563378+00:00 app[web.1]:  Starting process with command `bundle exec puma -C config/puma.rb` 2019-02-05T01:42:57.563241+00:00 app[web.1]: ! Unable to load application: ArgumentError: Before process_action callback :verify_authenticity_token has not been defined 2019-02-05T01:42:57.563378+00:00 app[web.1]: .logger.formatter = proc { |severity, datetime, progname, msg|
 #   original_formatter.call(severity, datetime, progname, msg.dump)
 # }
 

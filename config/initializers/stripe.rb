3283594c608dda7ea2a7bba80804772ecf40010c
ThomasConnolly <tom.connolly@comcast.net>
2019-02-08 12:26:15 -0500

    working on stripe using Oliver's tutorial

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,6 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
-}
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+Stripe.api_key = Rails.application.credentials.stripe_private_key
 
 # Stripe.api_key = Rails.configuration.stripe[:secret_key]
 # StripeEvent.signing_secret = Rails.application.credentials.stripe_webhook_secret

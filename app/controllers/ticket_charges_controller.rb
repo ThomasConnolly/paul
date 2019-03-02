b27ae8b0891ad2aee4a668b24e657370ca60089a
ThomasConnolly <tom.connolly@comcast.net>
2019-03-02 18:26:56 -0500

    still working on concert tickets

--- app/controllers/ticket_charges_controller.rb
+++ app/controllers/ticket_charges_controller.rb
@@ -8,24 +8,24 @@ class TicketChargesController < ApplicationController
     @amount = @ticket.amount
 
   customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
+    email: params[:stripeEmail],
+    source: params[:stripeToken]
+  )
+  Stripe::Charge.create(
+    customer: customer.id,
+    amount: @amount,
+    description: @ticket.event.title,
+    currency: 'usd'
   )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => @ticket.event.title,
-    :currency    => 'usd'
-  )
-
-    @ticket.update(email: params[:stripeEmail],
+  @ticket.update(email: params[:stripeEmail],
     source: params[:stripeToken], customer_id: customer.id)
-    @ticket.save
-    redirect_to root_path, notice: "Thank you!"
+  @ticket.save
 
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to root_path
-    end
+  redirect_to root_path, notice: 'Thank you!'
+
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  render action: :new
   end
+end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,42 +1,19 @@
-# == Schema Information
-#
-# Table name: tickets
-#
-#  id          :integer          not null, primary key
-#  price       :decimal(, )
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  event_id    :integer
-#  quantity    :integer
-#  amount      :integer
-#  first_name  :string
-#  last_name   :string
-#  email       :string
-#  customer_id :string
-#  source      :string
-#  honey       :string
-#
-
 class Ticket < ApplicationRecord
   belongs_to :event
   before_save :set_amount
   after_update :email_buyer
-  #honey used to prevent bots-filled forms from being saved to db
+#honey used to prevent bots-filled forms from being saved to db
   validates :honey, absence: true
 
   default_scope -> { order('last_name') }
 
-
-
   def set_amount
     self.amount = self.quantity * self.price
-   end
-
+  end
 
   private
 
   def email_buyer
     TicketMailer.thanks(self).deliver
   end
-
 end

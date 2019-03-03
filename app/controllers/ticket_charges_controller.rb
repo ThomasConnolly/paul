21daf20c113c388398d6213b41d7d7c763b902f2
ThomasConnolly <tom.connolly@comcast.net>
2019-03-02 19:08:07 -0500

    found issue is with mailer. working on that while silencing ticket popup

--- app/controllers/ticket_charges_controller.rb
+++ app/controllers/ticket_charges_controller.rb
@@ -22,7 +22,7 @@ class TicketChargesController < ApplicationController
     source: params[:stripeToken], customer_id: customer.id)
   @ticket.save
 
-  redirect_to root_path, notice: 'Thank you!'
+  redirect_to "/", notice: 'Thank you!'
 
 rescue Stripe::CardError => e
   flash[:error] = e.message

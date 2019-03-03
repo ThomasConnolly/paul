09cab7111385f8d846a8b314046ea7828b6b244a
ThomasConnolly <tom.connolly@comcast.net>
2019-03-02 19:58:01 -0500

    working on sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: 'SaintPaulsNaples',
+  password: Rails.application.credentials.sendgrid[:password],
+  domain: 'saintpaulsnaples.org',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: 'plain',
+  enable_starttls_auto: true
+}

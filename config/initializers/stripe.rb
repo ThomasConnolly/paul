aad874b81699ab0ebe332fec01280fd10f3e5c05
ThomasConnolly <tom.connolly@comcast.net>
2019-03-08 22:00:26 -0500

    switching to live stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
 
 Rails.configuration.stripe = {
   # First two are live keys, second two are test keys
-#  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-#  :secret_key      => ENV['STRIPE_SECRET_KEY']
-  publishable_key: Rails.application.credentials.stripe[:public_key],
-  secret_key: Rails.application.credentials.stripe[:private_key]
+ :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+ :secret_key      => ENV['STRIPE_SECRET_KEY']
+  # publishable_key: Rails.application.credentials.stripe[:public_key],
+  # secret_key: Rails.application.credentials.stripe[:private_key]
 }
 #Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-Stripe.api_key = Rails.application.credentials.stripe[:private_key]
+Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
 # StripeEvent.signing_secret = Rails.application.credentials.stripe_webhook_secret
 #
 # StripeEvent.configure do |config|

2422b7fe42ed44037ff96217047617997b11e678
ThomasConnolly <tom.connolly@comcast.net>
2019-03-12 12:08:38 -0400

    I think I have it now

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,7 +7,7 @@ Rails.configuration.stripe = {
 #  public_key: Rails.application.credentials.stripe[:publishable_key],
 #  secret_key: Rails.application.credentials.stripe[:secret_key]
 }
-#Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 #Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
 # StripeEvent.signing_secret = Rails.application.credentials.stripe_webhook_secret
 #

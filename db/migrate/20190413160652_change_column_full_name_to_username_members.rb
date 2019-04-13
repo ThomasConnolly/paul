c13ba60c687fc43b23da865c79bd87bad7f0ee7e
ThomasConnolly <tom.connolly@comcast.net>
2019-04-13 12:31:40 -0400

    changing member full_name to username

---
+++ db/migrate/20190413160652_change_column_full_name_to_username_members.rb
@@ -0,0 +1,5 @@
+class ChangeColumnFullNameToUsernameMembers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :members, :full_name, :username
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_023306) do
+ActiveRecord::Schema.define(version: 2019_04_13_160652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -180,7 +180,7 @@ ActiveRecord::Schema.define(version: 2019_03_01_023306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "membership_id"
-    t.string "full_name"
+    t.string "username"
   end
 
   create_table "opportunities", id: :serial, force: :cascade do |t|

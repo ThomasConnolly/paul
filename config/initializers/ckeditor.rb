bce3026af84408acc96414207cc5cda948c12cf2
ThomasConnolly <tom.connolly@comcast.net>
2019-05-21 09:47:52 -0400

    fix sermons style, update home page

--- config/initializers/ckeditor.rb
+++
@@ -1,5 +0,0 @@
-Ckeditor.setup do |config|
-  require 'ckeditor/orm/active_record'
-  config.js_config_url = 'ckeditor/config.js'
-end
-
--- public/assets/application-2b23e4a0cd18981cd2bcf5e2c46073dcc8c28f2edf90b1e334f4aab6f7977f95.js
+++ public/assets/application-2b23e4a0cd18981cd2bcf5e2c46073dcc8c28f2edf90b1e334f4aab6f7977f95.js
@@ -34099,41 +34099,6 @@ function stripeTokenHandler(token) {
    form.appendChild(hiddenInput);
  }
 });
-
-  var CKEDITOR_BASEPATH = '/assets/ckeditor/';
-CKEDITOR.editorConfig = function(config) 
-{
-  config.language = 'en';
-  config.width = "100%";
-  config.height = "100%";
-  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
-  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
-  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
-  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
-  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-
-  config.toolbar = [
-    { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source'] },
-    { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'] },
-    { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] }, 
-    { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight']},
-    { name: 'links', items: ['Link', 'Unlink']}, 
-    { name: 'insert', items: ['Table', 'HorizontalRule']},
-    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] }
-  ];
-};
-(function() {
-  var ready;
-
-  ready = function() {
-    $('.ckeditor').each(function() {
-      return CKEDITOR.replace($(this).attr('id'));
-    });
-    $(document).ready(ready);
-    return $(document).on('page:load', ready);
-  };
-
-}).call(this);
 (function() {
 
 

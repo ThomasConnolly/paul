cb5d577328c0cb327b96fea02c516a40ba34ad10
ThomasConnolly <tom.connolly@comcast.net>
2020-01-07 10:26:17 -0500

    multiple deletions and reinstallations of node, webpacker, etc.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
+environment.plugins.append('Provide', 
+  new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     jquery: 'jquery',
-    Popper: ['popper.js', 'default']
-})
+    Popper: ['popper.js', 'default'],
+    Rails: ['@rails/ujs']
+  })
 )
 
 module.exports = environment

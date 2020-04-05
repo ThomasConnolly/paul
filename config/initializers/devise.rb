9c8ceab5eb6b3625d17cae28c9f47e7798aaa3dd
ThomasConnolly <tom.connolly@comcast.net>
2020-04-04 23:59:19 -0400

    fixing 'lost password' response :host

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -119,7 +119,7 @@ Devise.setup do |config|
   # config.send_email_changed_notification = false
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
@@ -151,7 +151,7 @@ Devise.setup do |config|
   # end
 
   # Defines which key will be used when confirming an account
-  config.confirmation_keys = [:username]
+  config.confirmation_keys = [:email]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
@@ -210,7 +210,7 @@ Devise.setup do |config|
   # ==> Configuration for :recoverable
   #
   # Defines which key will be used when recovering the password for an account
-  config.reset_password_keys = [:login]
+  config.reset_password_keys = [:email]
 
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to

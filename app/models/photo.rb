a176bed6fc08aee480f520c2fcbed1c6f5f784c9
ThomasConnolly <tom.connolly@comcast.net>
2020-04-07 10:54:12 -0400

    had Photo class in models; deadened it in order to launch on heroku

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
-class Photo < ApplicationRecord
-  include ImageUploader::Attachment(:image)
+# class Photo < ApplicationRecord
+#   include ImageUploader::Attachment(:image)
 
-  validates :title, presence: true
-end
+#   validates :title, presence: true
+# end

6cae257d00d632a729bc83ad7948fe55f41ec921
ThomasConnolly <tom.connolly@comcast.net>
2020-04-10 10:16:12 -0400

    added cloud_name

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 Cloudinary.config do |config|
+  config.cloud_name = "saintpaulsnaples-org"
   config.api_key = ENV['CL_API_KEY']
   config.api_secret = ENV['CL_API_SECRET'] 
   config.secure = true

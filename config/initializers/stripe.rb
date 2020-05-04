bd85a644c9463d5a9a1b49ec3befde36dfdb5c98
ThomasConnolly <tom.connolly@comcast.net>
2020-05-04 15:38:31 -0400

    fixed syntax in stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,11 +5,11 @@ Rails.env.development?
  }
 
   Stripe.api_key = Rails.application.credentials.stripe[:private_key]
-else
+
+Rails.env.production?
   Rails.configuration.stripe = {
     publication_key: ENV[STRIPE_PUBLIATION_KEY],
     private_key: ENV[STRIPE_SECRET_KEY],
   }
   
   Stripe.api_key = ENV[STRIPE_SECRET_KEY]
-end

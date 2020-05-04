90156b8cef5cf532dd024168b0c3b7f606c5f2ee
ThomasConnolly <tom.connolly@comcast.net>
2020-05-04 15:50:22 -0400

    typo in config file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,15 +1,17 @@
-Rails.env.development?
+if Rails.env.development?
   Rails.configuration.stripe = {
     public_key: Rails.application.credentials.stripe[:public_key],
     private_key: Rails.application.credentials.stripe[:private_key],
  }
 
   Stripe.api_key = Rails.application.credentials.stripe[:private_key]
+end
 
-Rails.env.production?
+if Rails.env.production?
   Rails.configuration.stripe = {
-    publication_key: ENV[STRIPE_PUBLIATION_KEY],
+    publication_key: ENV[STRIPE_PUBLICATION_KEY],
     private_key: ENV[STRIPE_SECRET_KEY],
   }
   
   Stripe.api_key = ENV[STRIPE_SECRET_KEY]
+end

ea65fd9f2cb7d904fa98de72fa64a728c0152877
ThomasConnolly <tom.connolly@comcast.net>
2020-05-15 22:25:50 -0400

    found more typos in controllers working on for stripe checkout

--- app/controllers/ticket_charges_controller.rb
+++ app/controllers/ticket_charges_controller.rb
@@ -1,23 +1,12 @@
 class TicketChargesController < ApplicationController
-  Stripe.api_key = Rails.application.credentials.stripe[:private_key]
+
 
   def new; end
 
   def create
     @ticket = Ticket.find(params[:ticket_id])
   
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: [{
-        name: 'Ticket purchase',
-        description: @ticket.event.title,     
-        quantity: @ticket.quantity,
-        amount: @ticket.amount,
-        currency: 'usd',
-      }]
-    #  success_url: ticket_charges_path({CHECKOUT_SESSION_ID}),
-    #  cancel_url: new_event_ticket_path
-    )
+
   end
 end
 

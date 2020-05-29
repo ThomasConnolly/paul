91ac92116cd3bd9742e2af65ae0ba29e231d6b65
ThomasConnolly <tom.connolly@comcast.net>
2020-05-29 09:31:54 -0400

    working on stripe in production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,19 +1,19 @@
- if Rails.env.development?
-  Rails.configuration.stripe = {
-    public_key: Rails.application.credentials.stripe[:public_key],
-    private_key: Rails.application.credentials.stripe[:private_key],
-    signing_secret: Rails.application.credentials.stripe[:signing_secret]
- }
-  Stripe.api_key = Rails.application.credentials.stripe[:private_key]
- end
+#  if Rails.env.development?
+#   Rails.configuration.stripe = {
+#     public_key: Rails.application.credentials.stripe[:public_key],
+#     private_key: Rails.application.credentials.stripe[:private_key],
+#     signing_secret: Rails.application.credentials.stripe[:signing_secret]
+#  }
+#   Stripe.api_key = Rails.application.credentials.stripe[:private_key]
+#  end
 
-if Rails.env.production?
+#if Rails.env.production?
   Rails.configuration.stripe = {
     public_key: ENV["STRIPE_PUBLISHABLE_KEY"],
     private_key: ENV["STRIPE_SECRET_KEY"],
     signing_secret: ENV["SIGNING_SECRET"]
   }
   Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-end
+# end
 
-StripeEvent.signing_secret = Rails.application.credentials.stripe  [:signing_secret]
+# StripeEvent.signing_secret = Rails.application.credentials.stripe  [:signing_secret]

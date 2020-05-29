bda5eb5d221fee190138ba1f803817ddb5bbf6c2
ThomasConnolly <tom.connolly@comcast.net>
2020-05-29 12:30:39 -0400

    resetting stripe keys to production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -11,9 +11,10 @@
   Rails.configuration.stripe = {
     public_key: ENV["STRIPE_PUBLISHABLE_KEY"],
     private_key: ENV["STRIPE_SECRET_KEY"],
-    signing_secret: ENV["SIGNING_SECRET"]
+    signing_secret: ENV["STRIPE_SIGNING_SECRET"]
   }
   Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+
 # end
 
 # StripeEvent.signing_secret = Rails.application.credentials.stripe  [:signing_secret]

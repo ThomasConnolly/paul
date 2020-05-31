5bd962e3ba1aeccc762975f42ff51c33895e0f80
ThomasConnolly <tom.connolly@comcast.net>
2020-05-30 20:15:54 -0400

    something goofy with credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,18 +1,7 @@
- if Rails.env.development?
-  Rails.configuration.stripe = {
-    public_key: Rails.application.credentials.stripe[:public_key],
-    private_key: Rails.application.credentials.stripe[:private_key],
-    signing_secret: Rails.application.credentials.stripe[:signing_secret]
+Rails.configuration.stripe = {
+  public_key: Rails.application.credentials.stripe[:public_key],
+  private_key: Rails.application.credentials.stripe[:private_key],
+  signing_secret: Rails.application.credentials.stripe[:signing_secret]
  }
   Stripe.api_key = Rails.application.credentials.stripe[:private_key]
- end
 
-if Rails.env.production?
-  Rails.configuration.stripe = {
-    public_key: ENV["STRIPE_PUBLISHABLE_KEY"],
-    private_key: ENV["STRIPE_SECRET_KEY"],
-    signing_secret: ENV["STRIPE_SIGNING_SECRET"]
-  }
-  Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-
- end

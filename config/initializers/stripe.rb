a53347ae49b6b3b619aae079fc8ecd4864dd9be2
ThomasConnolly <tom.connolly@comcast.net>
2020-06-01 08:24:58 -0400

    getting close on Stripe Donations. Working in dev

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,10 @@
  if Rails.env.development?
   Rails.configuration.stripe = {
-    public_key: Rails.application.credentials.stripe[:public_key],
-    private_key: Rails.application.credentials.stripe[:private_key],
+    public_key: Rails.application.credentials.stripe[:publishable_key],
+    private_key: Rails.application.credentials.stripe[:secret_key],
     signing_secret: Rails.application.credentials.stripe[:signing_secret]
  }
-  Stripe.api_key = Rails.application.credentials.stripe[:private_key]
+  Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
  end
 
 if Rails.env.production?
@@ -13,5 +13,5 @@ if Rails.env.production?
     private_key: ENV["STRIPE_SECRET_KEY"],
     signing_secret: ENV["STRIPE_SIGNING_SECRET"]
   }
-  Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+  Stripe.api_key = ENV["STRIPE_SECRET_KEY"] 
  end

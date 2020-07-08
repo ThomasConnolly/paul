7f724ee5200b0e6b462cb67ad31db692fbbc455f
ThomasConnolly <tom.connolly@comcast.net>
2020-07-08 16:25:41 -0400

    birthday order not so good; typo in links template

--- app/helpers/birthdays_helper.rb
+++ app/helpers/birthdays_helper.rb
@@ -2,6 +2,6 @@
 module BirthdaysHelper
   def birthdays_this_week
     Member.find_birthdays_for(Date.today.beginning_of_week,
-                              Date.today.end_of_week).order(:birthday.yday)
+                              Date.today.end_of_week).order(:birthday)
   end
 end

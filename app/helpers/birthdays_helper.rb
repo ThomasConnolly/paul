2aa4ca888a2b967f5c85e2a516de807e6b2085a5
ThomasConnolly <tom.connolly@comcast.net>
2020-07-08 15:37:56 -0400

    added birthday helper for order

---
+++ app/helpers/birthdays_helper.rb
@@ -0,0 +1,7 @@
+
+module BirthdaysHelper
+  def birthdays_this_week
+    Member.find_birthdays_for(Date.today.beginning_of_week,
+                              Date.today.end_of_week).order(:birthday.yday)
+  end
+end

50a0f00e5c24fb3bc9139ca805c1612b63ea316c
ThomasConnolly <tom.connolly@comcast.net>
2020-09-26 22:42:13 -0400

    finishing touches on stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,14 +1,1 @@
-if Rails.env.development?
-  Rails.configuration.stripe = {
-    public_key: Rails.application.credentials.stripe[:publishable_key],
-    private_key: Rails.application.credentials.stripe[:secret_key],
-  }
-  Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
-
-elsif Rails.env.production?
-  Rails.configuration.stripe = {
-    public_key: ENV["STRIPE_PUBLISHABLE_KEY"],
-    private_key: ENV["STRIPE_SECRET_KEY"],
-  }
-  Stripe.api_key = ENV["STRIPE_SECRET_KEY"] 
-end
+Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:secret_key]

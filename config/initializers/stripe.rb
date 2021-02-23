55f172432c4b4323ce0c22e6204ad8f57e3227df
ThomasConnolly <tom.connolly@comcast.net>
2021-02-23 10:56:41 -0500

    still on strip creds

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
-Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:secret_key]
+Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]

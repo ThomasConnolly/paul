e268f5b42f47cc266faf8b77436ca731cd40a35a
ThomasConnolly <tom.connolly@comcast.net>
2021-02-25 07:06:12 -0500

    fixed sendgrid mail issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,15 +105,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'SaintPaulsNaples.org',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: ENV['SENDGRID_API'],
-    enable_starttls_auto: true,
-  }
-
 
 end

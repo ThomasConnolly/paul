5ce535cb2712f8f602485e380cf471eaf84aa254
ThomasConnolly <tom.connolly@comcast.net>
2021-05-01 09:17:33 -0400

    remove walkin shots notice

--- app/mailers/shot_mailer.rb
+++ app/mailers/shot_mailer.rb
@@ -1,6 +1,9 @@
 class ShotMailer < ApplicationMailer
   def reminder(shot)
     @shot = shot
+    @shot.first_name = shot.first_name
+    @shot.last_name = shot.last_name
+    @shot.shot_time = shot.shot_time
     mail( 
       to: -> { Shot.where.not(email: nil).pluck(:email) }, 
       subject: 'Reminder about your vaccination'

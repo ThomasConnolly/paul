01ef0ecafa6539bcc8934332cc9ac20b6854a0f1
ThomasConnolly <tom.connolly@comcast.net>
2021-10-29 08:55:23 -0400

    working on hotwire

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,6 +9,6 @@ window.Stimulus      = application
 
 // Import and register all your controllers within this directory and all subdirectories
 // Controller files must be named *_controller.js or *_controller.ts
-import { definitionsFromContext } from "@hotwired/stimulus"
+
 const context = require.context("controllers", true, /_controller\.(js|ts)$/)
 application.load(definitionsFromContext(context))

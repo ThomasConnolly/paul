46ee92a1aedfba4ce47d4645d3843e5f6047539c
ThomasConnolly <tom.connolly@comcast.net>
2022-04-08 09:44:38 -0400

    another typo

--- app/mailers/pathway_mailer.rb
+++ app/mailers/pathway_mailer.rb
@@ -4,8 +4,8 @@ class PathwayMailer < ApplicationMailer
   def pathway_created(pathway)
     @pathway = Pathway.last
     @url = url_for(@pathway)
-    mail(
-      to: { Member.where_not(email: nil).pluck(:email) },
-      subject: "There's a new Pathways")
+    mail(to: Member.where.Snot(email: nil).pluck(:email),
+         subject: "There's a new Pathways")
   end
 end
+

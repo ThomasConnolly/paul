2d7b5cb42e3ee85d6797edb5c3b9f132a3504a53
ThomasConnolly <tom.connolly@comcast.net>
2023-12-10 23:05:48 -0500

    narrowing down issue that's preventing deployment

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,5 +1,10 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'chat_channel'
+    stream_from "chat_#{params[:room]}"
+  end
+
+  def speak(data)
+    p data
+    ActionCable.server.broadcast("chat_#{params[:room]}", message: data['message'])
   end
 end

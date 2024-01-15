d83a191d5c52133e3c941213422ba41a8782176e
ThomasConnolly <tom.connolly@comcast.net>
2024-01-15 12:57:16 -0500

    fixing email host issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "paul_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  Rails.application.routes.default_url_options[:host] = 'www.saintpaulsnaples.org'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

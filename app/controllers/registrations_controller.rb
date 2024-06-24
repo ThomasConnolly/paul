21450c17a703490034dcd14ab4afad453994353c
ThomasConnolly <tom.connolly@comcast.net>
2024-06-24 10:03:25 -0400

    fixed name of registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RegistrationsController < Devise::RegistrationsControll
+class RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_sign_up_params

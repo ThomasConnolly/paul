3fa9a30fe2d453eb004a4548d80d8db89bc30175
Thomas Connolly <tom.connolly@comcast.net>
2024-07-24 10:22:26 -0400

    fixed ref to trix in js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import { Application } from "@hotwired/stimulus"
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
-import "Trix"
+import Trix from "trix"
 import "@rails/actiontext"
 import "@rails/actioncable"
 

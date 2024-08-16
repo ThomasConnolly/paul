bb3912bd06fa03eae118005d40fe84ac70322a7f
ThomasConnolly <tom.connolly@comcast.net>
2024-08-16 10:01:37 -0400

    working on webhooks; fixed geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -11,8 +11,8 @@ Geocoder.configure(
   http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   api_key: Rails.application.credentials.dig(:ipinfo, :api_key), # API key for geocoding service
-  cache: Redis.new,           # cache object (must respond to #[], #[]=, and #del)
-  cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
+  cache: $redis, # cache object (must respond to #[], #[]=, and #del)
+  cache_prefix: 'geocoder:', # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);

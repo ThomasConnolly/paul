c74af0956616be9d084f328a72fdbee97d2acf97
ThomasConnolly <tom.connolly@comcast.net>
2024-09-07 15:36:23 -0400

    still working on js side

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,6 @@
 import { Application } from "@hotwired/stimulus"
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
-import Trix from "trix"
-import "@rails/actiontext"
 import "@rails/actioncable"
-
 //Import controllers
 import "./controllers"
-
-import "trix"
-import "@rails/actiontext"

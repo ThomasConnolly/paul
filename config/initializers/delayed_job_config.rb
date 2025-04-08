dfb53e50055b50943b7570cd1a25011171a15909
Thomas Connolly <tom.connolly@comcast.net>
2025-04-08 13:45:30 -0400

    Making fixes to deploy to Heroku

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,3 +1,4 @@
+rb
 # frozen_string_literal: true
 
 Delayed::Worker.destroy_failed_jobs = false
@@ -7,4 +8,14 @@ Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.raise_signal_exceptions = :term
-Delayed::Worker.logger = Logger.new(Rails.root.join('log/delayed_job.log').to_s)
+
+# Configure logging based on environment
+if Rails.env.production? && ENV['DYNO'].present?
+  # Use STDOUT for Heroku
+  Delayed::Worker.logger = Logger.new(STDOUT)
+else
+  # Ensure log directory exists in other environments
+  log_dir = Rails.root.join('log')
+  FileUtils.mkdir_p(log_dir) unless File.directory?(log_dir)
+  Delayed::Worker.logger = Logger.new(Rails.root.join('log/delayed_job.log').to_s)
+end

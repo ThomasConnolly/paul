ec769506ed28d9287083a2d48136f988196a868d
Thomas Connolly <tom.connolly@comcast.net>
2025-04-08 14:28:50 -0400

    still fixing that fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,3 @@
 //= link_tree ../images
-//= link_tree ../stylesheets
-//= link application.js
-//= link application.css
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css

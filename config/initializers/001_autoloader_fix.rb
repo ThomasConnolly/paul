6aa72413f709eade4c851fadee0de61c3fc09ee9
Thomas Connolly <tom.connolly@comcast.net>
2025-04-10 08:04:33 -0400

    Now fixing the fixing another legacy issue

--- config/initializers/001_autoloader_fix.rb
+++ config/initializers/001_autoloader_fix.rb
@@ -7,7 +7,11 @@ if defined?(Rake) &&
    Rails.application.config.before_initialize do
      # Set up autoloaders if they're not initialized
      if Rails.autoloaders.respond_to?(:zeitwerk_enabled?) && Rails.autoloaders.zeitwerk_enabled?
-       Rails.autoloaders.instance_variable_get(:@main) ||= Zeitwerk::Loader.new
+       # Fixed syntax: Check if main is nil and assign if needed
+       main = Rails.autoloaders.instance_variable_get(:@main)
+       if main.nil?
+         Rails.autoloaders.instance_variable_set(:@main, Zeitwerk::Loader.new)
+       end
      end
    end
  end

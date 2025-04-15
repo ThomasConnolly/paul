3fc17f891dbc63d2cf0133f00210ddaaf9b80a18
Thomas Connolly <tom.connolly@comcast.net>
2025-04-15 08:43:15 -0400

    Nearing wit's end, part 2...

--- config/initializers/skip_credentials_in_assets.rb
+++ config/initializers/skip_credentials_in_assets.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # Skip credentials check during asset precompilation
-if ENV['RAILS_ENV'] == 'production' &&
-   defined?(Rake) &&
-   Rake.application.top_level_tasks.include?('assets:precompile')
+if ENV['RAILS_PRECOMPILE_ASSETS'] == 'true' || (defined?(Rake) && Rake.const_defined?(:Application) && 
+  Rake.application && Rake.application.top_level_tasks &&
+  Rake.application.top_level_tasks.include?('assets:precompile'))
 
   # Create a temporary mock for Rails.application.credentials
   module Rails

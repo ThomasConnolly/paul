5e532cb0858e7465219ce66fdf04f667baf8006b
Thomas Connolly <tom.connolly@comcast.net>
2025-04-15 07:46:58 -0400

    Now fixing the fixed fixes

--- config/initializers/010_propshaft_patches.rb
+++ config/initializers/010_propshaft_patches.rb
@@ -15,50 +15,53 @@ if defined?(Propshaft)
             Rails.root.join("app/assets", logical_path).to_s
           end
         end
-        
-        # Nested module for source mapping URLs - properly access it
-        if defined?(SourceMappingUrls)
-          module SourceMappingUrls
-            # Override source_mapping_url to be safe
-            alias_method :original_source_mapping_url, :source_mapping_url if method_defined?(:source_mapping_url)
+      end
+    end
+    
+    # Wait until Propshaft is fully loaded before patching the nested classes
+    Rails.application.config.after_initialize do
+      if defined?(Propshaft::Compiler::SourceMappingUrls)
+        # Patch the class using class_eval
+        Propshaft::Compiler::SourceMappingUrls.class_eval do
+          # Override source_mapping_url to be safe
+          alias_method :original_source_mapping_url, :source_mapping_url if method_defined?(:source_mapping_url)
+          
+          def source_mapping_url(logical_path)
+            return "" unless compiler
             
-            def source_mapping_url(logical_path)
-              return "" unless compiler
+            begin
+              path = compiler.load_path(logical_path)
+              return "" unless path && !path.empty?
               
-              begin
-                path = compiler.load_path(logical_path)
-                return "" unless path && !path.empty?
-                
-                if File.exist?(path)
-                  map_path = path + ".map"
-                  if File.exist?(map_path)
-                    File.join(compiler.url_prefix, logical_path + ".map")
-                  end
+              if File.exist?(path)
+                map_path = path + ".map"
+                if File.exist?(map_path)
+                  File.join(compiler.url_prefix, logical_path + ".map")
                 end
-              rescue => e
-                # Return empty string on error
-                ""
               end
+            rescue => e
+              # Return empty string on error
+              ""
             end
+          end
+          
+          # Override compile to be safe
+          alias_method :original_compile, :compile if method_defined?(:compile)
+          
+          def compile(logical_path, input)
+            return input unless input.is_a?(String)
             
-            # Override compile to be safe
-            alias_method :original_compile, :compile if method_defined?(:compile)
-            
-            def compile(logical_path, input)
-              return input unless input.is_a?(String)
-              
-              begin
-                input.gsub(/\/\/# sourceMappingURL=.+\n/m) do
-                  if url = source_mapping_url(logical_path)
-                    "//# sourceMappingURL=#{url}\n"
-                  else
-                    ""
-                  end
+            begin
+              input.gsub(/\/\/# sourceMappingURL=.+\n/m) do
+                if url = source_mapping_url(logical_path)
+                  "//# sourceMappingURL=#{url}\n"
+                else
+                  ""
                 end
-              rescue => e
-                # Return original input on error
-                input
               end
+            rescue => e
+              # Return original input on error
+              input
             end
           end
         end

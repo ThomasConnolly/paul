730ec3363b075e152e1d9ab54eee2793795e80bb
Thomas Connolly <tom.connolly@comcast.net>
2025-04-22 12:02:29 -0400

    final fix I hope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,9 +25,9 @@ class ApplicationController < ActionController::Base
   end
 
   def verified_request?
-    super || valid_authenticity_token?(session,
-          request.headers['X-CSRF-Token']) 
-          || request.headers['X-Requested-With'] == 'Turbo-Frame'
+    super || 
+      valid_authenticity_token?(session, request.headers['X-CSRF-Token']) || 
+      request.headers['X-Requested-With'] == 'Turbo-Frame'
   end
 
   private

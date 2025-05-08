ea0314d542a72b3480021e021f08b9835f7fd570
Thomas Connolly <tom.connolly@comcast.net>
2025-05-08 17:31:38 -0400

    Precompiled assets worked

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = ENV.fetch('SENTRY_DSN', nil) # Uses the DSN from Heroku
+  config.dsn = ENV.fetch('SENTRY_DSN', nil)
   config.environment = Rails.env # Tracks errors differently in dev vs. prod
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.send_default_pii = true # Capture user data if needed
 end
-paul(prod)> Sentry.capture_message("Test error from Heroku!")
-(paul):1:in '<main>': uninitialized constant Sentry (NameError)paul(prod)> Sentry.capture_message("Test error from Heroku!")
-(paul):1:in '<main>': uninitialized constant Sentry (NameError)

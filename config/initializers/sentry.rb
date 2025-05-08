3aba32956dc8d4f46f4b4ddf403afb1ef1ee51f2
Thomas Connolly <tom.connolly@comcast.net>
2025-05-08 16:18:03 -0400

    setup dyno & repushing Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,3 +6,6 @@ Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.send_default_pii = true # Capture user data if needed
 end
+paul(prod)> Sentry.capture_message("Test error from Heroku!")
+(paul):1:in '<main>': uninitialized constant Sentry (NameError)paul(prod)> Sentry.capture_message("Test error from Heroku!")
+(paul):1:in '<main>': uninitialized constant Sentry (NameError)
